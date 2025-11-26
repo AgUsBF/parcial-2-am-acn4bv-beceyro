c7ff00eae92bade5c50fb16256c2dc5caf9d184c
AgUsBF <agustinbeceyro@gmail.com>
2025-11-26 09:57:19 -0300

    feat: agregar DetalleProductoActivity y su layout

---
+++ app/src/main/java/com/example/viverodv/DetalleProductoActivity.java
@@ -0,0 +1,33 @@
+package com.example.viverodv;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import android.content.Intent;
+import com.example.viverodv.model.Producto;
+
+public class DetalleProductoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_producto);
+
+        
+        Producto producto = (Producto) getIntent().getSerializableExtra("producto");
+        
+        TextView saludo = findViewById(R.id.saludo);
+        saludo.setText(producto.getNombre());
+    }
+}
